9512b9bbe0d5aa5b9a3bcfb7b59dba5dd39869cf
Reperi-toxi <gigicho.ch@gmail.com>
2026-02-23 21:45:20 +0400

    temp solution to severely disturbing sounds during dragging the slider

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from PyQt6.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton,
 from music_player import MusicPlayer
 from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtCore import Qt, QTimer
+import time
 
 class MainWindow(QMainWindow):
-    width = 700 # size of main window
+    width = 700  # size of main window
     height = 600
 
     def __init__(self):
@@ -18,17 +19,17 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("Background-Color: rgb(8, 6, 59)")
         # Main blocks of app ---------------------------------------------------------
         self.timer: QTimer = QTimer()
-        self.central_widget = QWidget() # all bellow will be part of central
+        self.central_widget = QWidget()  # all bellow will be part of central
         self.title_widget = QWidget()
         self.track_container_widget = QWidget()
         self.buttons_widget = QWidget()
         self.volume_container_widget = QWidget()
-        self.music_list_widget : QListWidget = QListWidget() # type hints to avoid warnings, annoying ah
+        self.music_list_widget: QListWidget = QListWidget()  # type hints to avoid warnings, annoying ah
         # Buttons labels and etc. ----------------------------------------------------
-        self.play_button : QPushButton = QPushButton("Play", self.buttons_widget)
+        self.play_button: QPushButton = QPushButton("Play", self.buttons_widget)
         self.stop_button: QPushButton = QPushButton("Stop", self.buttons_widget)
-        self.forward_button : QPushButton = QPushButton("+5", self.buttons_widget)
-        self.backward_button : QPushButton = QPushButton("-5", self.buttons_widget)
+        self.forward_button: QPushButton = QPushButton("+5", self.buttons_widget)
+        self.backward_button: QPushButton = QPushButton("-5", self.buttons_widget)
 
         self.main_label = QLabel("Music Player", self.title_widget)
 
@@ -47,12 +48,12 @@ class MainWindow(QMainWindow):
         self.button_layout = QHBoxLayout()
         self.volume_layout = QHBoxLayout()
         self.music_list_layout = QVBoxLayout()
-        # Assign to layouts -----------------------------------------
+        # Assign to layouts ------------------------------------------------
         # add widgets to layouts
         self.main_layout.addWidget(self.title_widget, stretch=1)
-        self.main_layout.addWidget(self.track_container_widget, stretch= 2)
+        self.main_layout.addWidget(self.track_container_widget, stretch=2)
         self.main_layout.addWidget(self.buttons_widget, stretch=3)
-        self.main_layout.addWidget(self.volume_container_widget, stretch = 2)
+        self.main_layout.addWidget(self.volume_container_widget, stretch=2)
         self.main_layout.addWidget(self.music_list_widget, stretch=8)
 
         self.title_layout.addWidget(self.main_label)
@@ -79,6 +80,7 @@ class MainWindow(QMainWindow):
 
         self.music_list = self.player.music_files
         self.play_from_beginning = True
+        self.is_dragging = False
         self.handle_song_labels()
 
         self.init_ui()
@@ -97,7 +99,7 @@ class MainWindow(QMainWindow):
         self.total_time_label.setFont(QFont("", 20))
 
         self.track_slider.setOrientation(Qt.Orientation.Horizontal)
-        self.track_slider.setRange(0, 100) # arbitrary, updated later with timer
+        self.track_slider.setRange(0, 100)  # arbitrary, updated later with timer
         self.track_slider.setValue(0)
         self.track_slider.setStyleSheet("""
                     QSlider::groove:horizontal {
@@ -119,6 +121,8 @@ class MainWindow(QMainWindow):
                     }
                 """)
         self.track_slider.valueChanged.connect(lambda value: self.on_change_track(value))
+        #self.track_slider.sliderPressed.connect(self.on_track_slider_pressed)
+        #self.track_slider.sliderReleased.connect(self.on_track_slider_released)
         # -- Buttons --------------------------------------------------------
         self.buttons_widget.setStyleSheet("""
             QPushButton {
@@ -127,11 +131,11 @@ class MainWindow(QMainWindow):
                 color: rgb(4, 43, 94);
                 background-color: white;
             }
-    
+
             QPushButton:hover {
                 background-color: rgb(230, 230, 230);
             }
-    
+
             QPushButton:pressed {
                 background-color: rgb(4, 43, 94);
                 color: white;
@@ -169,7 +173,6 @@ class MainWindow(QMainWindow):
                 border-radius: 3px;
             }
         """)
-
         self.volume_slider.valueChanged.connect(lambda value: self.on_change_volume(value / 50))
         # -- List of songs widget ----------------------------------------------------------
         self.music_list_widget.setStyleSheet("""
@@ -201,11 +204,12 @@ class MainWindow(QMainWindow):
                 background-color: #2c2cf5;
             }
         """)
-        #self.music_list_widget.clicked.connect(self.on_click_song)
+        # self.music_list_widget.clicked.connect(self.on_click_song)
+
     def set_timer(self):
         self.timer.setInterval(200)  # checks every 0.2 seconds
-        self.timer.timeout.connect(self.check_song_end) # check whether song has ended
-        self.timer.timeout.connect(self.update_time_label) # update current time label
+        self.timer.timeout.connect(self.check_song_end)  # check whether song has ended
+        self.timer.timeout.connect(self.update_time_label)  # update current time label
         self.timer.timeout.connect(self.update_track_slider)
         self.timer.start()
 
@@ -213,12 +217,23 @@ class MainWindow(QMainWindow):
         pos_seconds = self.player.get_position() or 0.0
         minutes = int(pos_seconds // 60)
         seconds = int(pos_seconds % 60)
-        time_str = f"{minutes}:{seconds:02}" # formatting to minutes:seconds, :02 to maek it two digit
+        time_str = f"{minutes}:{seconds:02}"  # formatting to minutes:seconds, :02 to maek it two digit
         self.current_time_label.setText(time_str)
+
+    # -- handling track slider -----------------------------------
+    def on_track_slider_pressed(self):
+        self.is_dragging = True
+    def on_track_slider_released(self):
+        QTimer.singleShot(200, self.after_track_slider_released)
+    def after_track_slider_released(self):
+        self.player.play(position=self.track_slider.value())
+        self.is_dragging = False
     def update_track_slider(self):
-        self.track_slider.blockSignals(True)
-        self.track_slider.setValue(int(self.player.get_position()))
-        self.track_slider.blockSignals(False)
+        if self.is_dragging:
+            self.track_slider.blockSignals(True)
+            self.track_slider.setValue(int(self.player.get_position()))
+            self.track_slider.blockSignals(False)
+    # -------------------------------------------------------------
     def check_song_end(self):
         if not self.player.get_busy() and self.player.is_playing and not self.player.is_paused:
             # song ended naturally
@@ -226,6 +241,7 @@ class MainWindow(QMainWindow):
             self.play_from_beginning = True
             self.play_button.setText("Play")
             self.main_label.setText("Music Player")
+
     def on_click_play(self):
         if self.play_from_beginning:
             current_song = self.music_list_widget.currentItem()
@@ -251,12 +267,14 @@ class MainWindow(QMainWindow):
             else:
                 self.player.pause()
                 self.play_button.setText("Resume")
+
     def on_click_stop(self):
         self.player.stop()
         self.play_from_beginning = True
         self.play_button.setText("Play")
         self.main_label.setText("Music Player")
         self.total_time_label.setText("0:00")
+
     def on_click_forward(self):
         if self.player.is_paused:
             self.play_button.setText("Pause")
@@ -266,17 +284,22 @@ class MainWindow(QMainWindow):
         if self.player.is_paused:
             self.play_button.setText("Pause")
         self.player.go_back()
+
     def on_click_song(self):
         self.player.stop()
         self.play_from_beginning = True
         self.play_button.setText("Play")
+
     def on_change_volume(self, volume):
         self.player.set_volume(volume)
+
     def on_change_track(self, value):
+        time.sleep(0.2)
         self.player.play(position=value)
     def handle_song_labels(self):
         for song in self.music_list:
             self.music_list_widget.addItem(song.removesuffix(".mp3"))
+
 def main():
     app = QApplication([])
     window = MainWindow()
